447e212e6075f18f10758253c8932b975348a674
Vladimir Demonow <vladimirdemonow@gmail.com>
2022-02-04 17:09:23 +0300

    Make taskListSlice code better

--- src/features/slices/taskListSlice.ts
+++ src/features/slices/taskListSlice.ts
@@ -25,23 +25,18 @@ const initialState: ITaskListState = {
 export const taskListSlice = createSlice({
   name: "taskList",
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     createTask: (state, action: PayloadAction<ITask>) => {
       state.tasks.push(action.payload);
     },
     completeTask: (state, action: PayloadAction<string>) => {
       state.tasks.find((element, index, array) => {
-        if (element.id === action.payload) {
-          array[index].isCompleted = true;
-        }
+        if (element.id === action.payload) array[index].isCompleted = true;
       });
     },
     uncompleteTask: (state, action: PayloadAction<string>) => {
       state.tasks.find((element, index, array) => {
-        if (element.id === action.payload) {
-          array[index].isCompleted = false;
-        }
+        if (element.id === action.payload) array[index].isCompleted = false;
       });
     },
     deleteTask: (state, action: PayloadAction<string>) => {
@@ -54,7 +49,6 @@ export const taskListSlice = createSlice({
         if (element.id === action.payload.id) {
           element.text = action.payload.text;
         }
-
         return element;
       });
     },
@@ -69,9 +63,6 @@ export const {
   changeTask,
 } = taskListSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectTaskList = (state: RootState) => state.taskList.tasks;
 
 export default taskListSlice.reducer;
