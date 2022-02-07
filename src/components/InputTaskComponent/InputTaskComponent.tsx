661c89cb76d00db5dfda553be0c7c94094353a79
Vladimir Demonow <vladimirdemonow@gmail.com>
2022-02-07 12:06:25 +0300

    Add counter of input task

--- src/components/InputTaskComponent/InputTaskComponent.tsx
+++ src/components/InputTaskComponent/InputTaskComponent.tsx
@@ -1,25 +1,29 @@
-import { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./InputTaskComponent.module.scss";
 import { v4 } from "uuid";
 
 import { useAppDispatch } from "../../app/hooks";
 import { createTask } from "../../features/slices/taskListSlice";
 
-interface IInputKey {
-  key: string;
-}
+const maxInputCount = 100;
 
 export default (): JSX.Element => {
   const dispatch = useAppDispatch();
+  const [countLeftInputs, setCountLeftInputs] = useState(maxInputCount);
 
   const inputTaskRef: any = useRef(null); // any, because inputTaskRef.current.value - Property 'value' does not exist on type 'never'
 
-  const onInputTask = ({ key }: IInputKey) => {
+  const onInputTask = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const key = event.key;
+
+    if (key === "Backspace") return;
+
     if (key === "Enter") {
       let { value } = inputTaskRef.current;
       inputTaskRef.current.value = "";
+      setCountLeftInputs(maxInputCount);
 
-      if (!value || value.length > 100) {
+      if (!value || value.length > maxInputCount) {
         return;
       }
 
@@ -34,11 +38,23 @@ export default (): JSX.Element => {
           timeStamp: Date.now(),
         })
       );
+
+      return;
     }
 
     if (key === "Escape") {
       inputTaskRef.current.value = "";
+      setCountLeftInputs(maxInputCount);
     }
+
+    if (inputTaskRef.current.value.length >= maxInputCount) {
+      event.preventDefault();
+      return;
+    }
+  };
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCountLeftInputs(maxInputCount - inputTaskRef.current.value.length);
   };
 
   return (
@@ -47,11 +63,17 @@ export default (): JSX.Element => {
         type="input"
         ref={inputTaskRef}
         onKeyDownCapture={onInputTask}
+        onChange={onChangeInput}
         className={styles.form__field}
         placeholder="new task"
         name="name"
         id="name"
       />
+      {countLeftInputs < maxInputCount ? (
+        <div className={styles.left_counter}>{countLeftInputs}</div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
