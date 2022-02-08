cd26ccece5bd9d9e64b86a0dcad0933678fce589
Vladimir Demonow <vladimirdemonow@gmail.com>
2022-02-08 19:16:24 +0300

    Start creating TaskApi

--- src/components/SortComponent/SortComponent.tsx
+++ src/components/SortComponent/SortComponent.tsx
@@ -4,6 +4,7 @@ import { AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { setSort, selectSort, TSort } from "../../features/slices/orderSlice";
 import { IconType } from "react-icons/lib";
+import { getTasksRequest, postTaskRequest } from "../../api/Task/TaskAPI";
 
 export default (): JSX.Element => {
   const sortSelector = useAppSelector(selectSort);
@@ -24,6 +25,13 @@ function createSortButton(sortSelector: TSort, dispatch: any) {
       size={35}
       onClick={() => {
         dispatch(setSort(sortSelector === "up" ? "down" : "up"));
+        // getTasksRequest();
+        postTaskRequest({
+          name: "string111",
+          done: true,
+          createdAt: "2022-02-08T15:35:53.815Z",
+          updatedAt: "2022-02-08T15:35:53.815Z",
+        });
       }}
     />
   );
